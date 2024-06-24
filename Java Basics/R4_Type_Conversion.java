ae2041c3fcd5b12c637454023bb67cfa3e87b93a
Rohan Dash <141203068+rohandashrd@users.noreply.github.com>
2024-06-24 11:24:18 +0530

    R4_Type_Conversion.java
    The provided Java code demonstrates type conversion between primitive data types. It shows how an integer (`int`) can be implicitly converted to a long (`long`) without any explicit cast. The variable `a` of type `int` is assigned the value 10 and then implicitly converted to a `long` and stored in the variable `b`. Both `b` and `a` are printed, illustrating that the value of `a` remains unchanged while `b` holds the same value as `a` but as a `long`. Additionally, there's a commented-out section showing an attempt to convert a `long` to an `int`, which would require an explicit cast and is not allowed implicitly, highlighting potential type conversion issues.

---
+++ Java Basics/R4_Type_Conversion.java
@@ -0,0 +1,14 @@
+public class R4_type_conversion {
+    public static void main(String[] args) {
+        int a= 10;
+        long b=a;
+        System.out.println(b);
+        System.out.println(a);
+
+        /*
+         long s=1528;
+         int c= s;
+         System.out.println(c);
+        */
+    }
+}
