5771958013e569de52e594617fabbc469a3997d3
Rohan Dash <141203068+rohandashrd@users.noreply.github.com>
2024-06-24 11:25:46 +0530

    R5_Type_Casting.java
    The provided Java code demonstrates type casting between different primitive data types. It first includes a commented-out section that shows an illegal cast from `float` to `int` without explicit casting. Then, it demonstrates explicit casting by converting a `float` value (99.99f) to an `int`, which truncates the decimal part and prints the result (99). Additionally, it shows how a `char` can be implicitly cast to an `int`, printing the Unicode value of the character 'A' (which is 65). This code illustrates both explicit and implicit type casting in Java.

---
+++ Java Basics/R5_Type_Casting.java
@@ -0,0 +1,15 @@
+public class R5_type_casting {
+    public static void main(String[] args) {
+            /*
+            float k=99.99f;
+            int g=k;
+            */
+
+        int m=(int) (99.99f);
+        System.out.println(m);
+
+        char ch='A';
+        int n=ch;
+        System.out.println(n);
+    }
+}
