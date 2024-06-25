d955b8c47e8ded400a9aa0d346524f87ee46757d
Rohan Dash <141203068+rohandashrd@users.noreply.github.com>
2024-06-25 11:18:48 +0530

    Add files via upload
    The provided Java code demonstrates the use of functions (or methods) in Java. It includes a method `printhello` that prints "Hello World" five times and a method `addition` that takes two integers as parameters, calculates their sum, and prints the result. In the `main` method, the program prompts the user to input two integers, which are then passed to the `addition` method to print their sum. The program also asks the user to enter either "T" or "F". If the user enters "T", the `printhello` method is called, resulting in "Hello World" being printed five times. This code effectively illustrates how to define and use functions in Java for repetitive tasks and specific calculations.

---
+++ 3. Functions/R10_Functions.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class R10_Functions {
+
+    public static void printhello(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+
+    public static void addition(int a , int b){
+        int sum=a+b;
+        System.out.println("Addition is : "+ sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int a =sc.nextInt();
+        int b=sc.nextInt();
+        addition(a,b);
+        System.out.print("Enter T or F: ");
+        String ch=sc.next();
+        if (ch.equals("T")){
+            printhello();
+        }
+
+
+    }
+}
