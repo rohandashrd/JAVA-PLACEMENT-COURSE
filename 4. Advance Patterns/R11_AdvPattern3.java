8da11250e635a10804e341650222f45e46288c57
Rohan Dash <141203068+rohandashrd@users.noreply.github.com>
2024-07-01 15:54:06 +0530

    Add files via upload
    The provided Java code generates a left-aligned numerical pattern where each row contains numbers in descending order from the row number down to 1. The program prompts the user to input the number of rows (`n`), and based on this input, it constructs the pattern. For example, if the user enters 5, the output will be:
    1 2 3 4 5 
    1 2 3 4 
    1 2 3 
    1 2 
    1 
    
    Code Explanation:
    1. User Input:
       - The program uses a `Scanner` object to prompt the user to enter the number of rows (`n`) for the pattern.
    
    2. Pattern Generation:
       - The outer loop (`for(int i=n; i>=1; i--)`) iterates from `n` down to 1. This loop controls the number of rows in the pattern.
       - The inner loop (`for(int j=1; j<=i; j++)`) iterates from 1 to `i`. This loop prints the numbers in each row, starting from 1 up to the current value of `i`.
    
    3. Output:
       - Inside the inner loop, numbers are printed followed by a space (`j + " "`).
       - After printing all numbers for a row, the inner loop terminates with a newline (`System.out.println()`), moving to the next line to print the next row.
    
    This code effectively demonstrates how to use nested loops to generate a specific numerical pattern in Java based on user input.

---
+++ 4. Advance Patterns/R11_AdvPattern3.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class R11_AdvPattern3 {
+
+//            12345
+//            1234
+//            123
+//            12
+//            1
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n= sc.nextInt();
+
+        //outer row
+        for(int i=n;i>=1;i--){
+             for(int j=1;j<=i;j++){
+                 System.out.print(j+" ");
+             }
+            System.out.println();
+         }
+    }
+}
